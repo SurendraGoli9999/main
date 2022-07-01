c4977f135b4ce9ff5fcf42d813856735c03c4d43
SurendraGoli <surendragoli2000@gmail.com>
2022-07-01 12:47:33 +0530

    file added

---
+++ chrome1.sql
@@ -0,0 +1,25 @@
+
+
+create database shopping;
+
+use shopping;
+
+create table grocery (gid int primary key auto_increment,item varchar(55),price int);
+
+select * from grocery;
+
+create database websparrow;
+
+use websparrow;
+create table USER_DATA( USER_ID varchar(55) ,USER_PASS varchar(55));
+
+select*from USER_DATA;
+
+create database web;
+
+use web;
+
+ create table userdata( usreid varchar(55) , userpass varchar(55));
+
+select*from userdata;
+
